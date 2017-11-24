f024ba404c8e6ee7fbe22fcdc46ab9196473adf8
Nick Krichevsky <nick@ollien.com>
2017-11-24 18:05:23 -0500

    Fix i3config install path in install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ function readLocations() {
 getopts "f" force
 declare -A locations=(["zshrc"]="$HOME/.zshrc"\
 					  ["vimrc"]="$HOME/.vimrc"\
-					  ["i3-configs"]="$HOME/.config/i3/config"\
+					  ["i3-config"]="$HOME/.config/i3/config"\
 					  ["terminalrc"]="$HOME/.config/xfce4/terminal/terminalrc"\
 					  ["gtkrc-2.0"]="$HOME/.gtkrc-2.0"\
 					  ["scripts"]="$HOME/.local/dotfile-scripts")
