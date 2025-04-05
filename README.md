0ada0cc8af3b7b0899a911b66abc52c7dbcba6fe
Nick Krichevsky <nick@ollien.com>
2025-04-05 14:55:00 -0400

    Add bootstrap script

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Dotfiles
 
-Yet another dotfiles repo... but this one is mine. Managed with chezmoi
+Yet another dotfiles repo... but this one is mine. 
+
+## Installation
+
+I use `chezmoi` to manage these dotfiles, but have included a bootstrap script
+to set things up on a fresh system. Ansible is required to run this script.
+
+
+```
+./bootstrap.sh
+```
---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [ ! $(which ansible-playbook) ]; then
+  echo "Ansible is required to bootstrap this machine." >&2
+  exit 1
+fi
+
+echo "Running Bootstrap Playbook..." >&2
+ANSIBLE_LOCALHOST_WARNING=False ansible-playbook -K bootstrap/bootstrap.yml
+
+echo "Applying Chezmoi..." >&2
+SOURCE_DIR=$(dirname $(realpath $BASH_SOURCE))
+chezmoi --source "$SOURCE_DIR" apply
