7d66a6a0d7bbaaae1d543e79d1f4014e45ba6574
Nick Krichevsky <nick@ollien.com>
2025-06-08 16:29:28 -0400

    Change directory when opening one

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -7,6 +7,10 @@ vim.api.nvim_create_autocmd("VimEnter", {
 		local directory = vim.fn.isdirectory(data.file) == 1
 
 		if no_name or directory then
+			if directory then
+				vim.cmd("cd " .. vim.fn.fnameescape(data.file))
+			end
+
 			require("nvim-tree.api").tree.open()
 		end
 	end,
