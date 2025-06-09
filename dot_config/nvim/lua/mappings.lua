4f8267f2fe609bd6c356c27bc3f092ffdda2c5da
Nick Krichevsky <nick@ollien.com>
2025-06-08 21:36:17 -0400

    Add alias for <leader>o for openinv telescope

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -1,12 +1,17 @@
 require("nvchad.mappings")
 
-local grug_find = require("config_util.grug_find")
+local grug_find = require("config_util.grug_far")
 
 local map = vim.keymap.set
 
+local function find_files()
+	require("telescope.builtin").find_files({ hidden = true })
+end
+
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map("n", "<leader>ff", "<cmd>Telescope find_files hidden=true<cr>", { desc = "telescope find files" })
+map("n", "<leader>o", find_files, { desc = "telescope find files" })
+map("n", "<leader>ff", find_files, { desc = "telescope find files" })
 map("n", "<leader>fk", require("telescope.builtin").keymaps, { noremap = true, desc = "telescope find keymaps" })
 map("n", "<leader>fc", require("telescope.builtin").commands, { noremap = true, desc = "telescope find commands" })
 map("n", "<leader>fr", require("telescope.builtin").resume, { noremap = true, desc = "telescope resume" })
