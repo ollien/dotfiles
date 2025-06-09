0be042f7b625cceb425617e06e29373ddec97708
Nick Krichevsky <nick@ollien.com>
2025-06-09 09:18:59 -0400

    Add comment highlighting

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -139,4 +139,16 @@ return {
 		opts = {},
 		keys = { "<leader>gy" },
 	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			highlight = {
+				before = "fg",
+				keyword = "fg",
+				after = "fg",
+			},
+		},
+		event = "BufCreate",
+	},
 }
