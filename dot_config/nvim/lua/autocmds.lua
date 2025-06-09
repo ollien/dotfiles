d504788b529d2230eb29e9d9713f16ddb069e7e8
Nick Krichevsky <nick@ollien.com>
2025-06-09 10:20:26 -0400

    Don't show the file picker if we're not opening anything

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -2,8 +2,6 @@ require("nvchad.autocmds")
 
 vim.api.nvim_create_autocmd("VimEnter", {
 	callback = function(data)
-		-- Only open if no file was opened or if opening a directory
-		local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
 		local directory = vim.fn.isdirectory(data.file) == 1
 
 		if no_name or directory then
