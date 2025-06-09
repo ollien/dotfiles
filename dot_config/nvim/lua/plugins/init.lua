26b8f125dcd5f8c18a606faafb0ca50acc2601b3
Nick Krichevsky <nick@ollien.com>
2025-06-09 00:18:14 -0400

    Add gitlinker

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -131,4 +131,12 @@ return {
 		priority = 100,
 		event = "VeryLazy",
 	},
+	{
+		"ruifm/gitlinker.nvim",
+		dependencies = {
+			{ "nvim-lua/plenary.nvim" },
+		},
+		opts = {},
+		keys = { "<leader>gy" },
+	},
 }
