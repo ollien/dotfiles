0460d2b3334167fe90ca8550c5146963f60ea6fd
Nick Krichevsky <nick@ollien.com>
2025-06-08 23:49:20 -0400

    Add neovim-project

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -111,4 +111,24 @@ return {
 			},
 		},
 	},
+	{
+		"coffebar/neovim-project",
+		opts = {
+			-- To be overridden per machine
+			projects = {},
+			picker = {
+				type = "telescope",
+			},
+		},
+		init = function()
+			vim.opt.sessionoptions:append("globals")
+		end,
+		dependencies = {
+			{ "nvim-lua/plenary.nvim" },
+			{ "nvim-telescope/telescope.nvim", tag = "0.1.4" },
+			{ "Shatur/neovim-session-manager" },
+		},
+		priority = 100,
+		event = "VeryLazy",
+	},
 }
