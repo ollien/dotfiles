d8794166837e70aca237c8a3a289e825897710de
Nick Krichevsky <nick@ollien.com>
2025-06-09 10:28:41 -0400

    Disable completions in comments

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -11,7 +11,17 @@ return {
 		end,
 	},
 	{
-		"cpea2506/one_monokai.nvim",
+		"hrsh7th/nvim-cmp",
+		opts = {
+			enabled = function()
+				local disabled = false
+				disabled = disabled or (vim.api.nvim_get_option_value("buftype", { buf = 0 }) == "prompt")
+				disabled = disabled or (vim.fn.reg_recording() ~= "")
+				disabled = disabled or (vim.fn.reg_executing() ~= "")
+				disabled = disabled or require("cmp.config.context").in_treesitter_capture("comment")
+				return not disabled
+			end,
+		},
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
