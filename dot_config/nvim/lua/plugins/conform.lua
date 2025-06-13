e94dcf2d2ab6c634e8ae5895352176826dd9bfc8
Nick Krichevsky <nick@ollien.com>
2025-06-13 09:29:17 -0400

    Move conform config to plugin dir

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -1,5 +1,15 @@
 return {
 	"stevearc/conform.nvim",
 	event = "BufWritePre",
-	opts = require("configs.conform"),
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			elixir = { "mix" },
+		},
+
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+	},
 }
