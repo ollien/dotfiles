96cf6d864201a999f223351b5d11f74260b70feb
Nick Krichevsky <nick@ollien.com>
2025-06-13 09:39:16 -0400

    Remove unused navic plugin

--- dot_config/nvim/lua/configs/lspconfig.lua
+++ dot_config/nvim/lua/configs/lspconfig.lua
@@ -1,7 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local config_util = require("config_util.require_all")
-local navic = require("nvim-navic")
 
 local servers = { "html", "cssls", "rust_analyzer", "gleam" }
 vim.lsp.enable(servers)
--- dot_config/nvim/lua/plugins/nvim-navic.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"SmiteshP/nvim-navic",
-	event = "BufEnter",
-	options = {
-		lsp = {
-			auto_attach = true,
-		},
-	},
-}
