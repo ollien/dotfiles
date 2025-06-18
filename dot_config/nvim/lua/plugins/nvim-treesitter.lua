1c201a77363b1239b6ef1b77e557487f5ef6c01b
Nick Krichevsky <nick@ollien.com>
2025-06-18 09:14:40 -0400

    Revert NVChad update

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,23 +1,37 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 
-	opts = {
-		ensure_installed = {
-			"css",
-			"elixir",
-			"erlang",
-			"gleam",
-			"html",
-			"json",
-			"lua",
-			"markdown",
-			"markdown_inline",
-			"python",
-			"rust",
-			"toml",
-			"vim",
-			"vimdoc",
-			"yaml",
-		},
-	},
+	opts = function(opts)
+		-- stupid workaround for the fact that the gleam parser broke with the new nvim-treesitter
+		-- stolen from https://github.com/rawhat/config/blob/master/config/nvim/lua/plugins/treesitter.lua#L94
+		local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+		parser_config.gleam = {
+			install_info = {
+				url = "https://github.com/gleam-lang/tree-sitter-gleam",
+				revision = "main",
+				files = { "src/parser.c", "src/scanner.c" },
+			},
+			filetype = "gleam",
+		}
+
+		vim.tbl_deep_extend("force", opts, {
+			ensure_installed = {
+				"css",
+				"elixir",
+				"erlang",
+				"gleam",
+				"html",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"rust",
+				"toml",
+				"vim",
+				"vimdoc",
+				"yaml",
+			},
+		})
+	end,
 }
