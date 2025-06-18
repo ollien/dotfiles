f47ebdf4c9b06100863491dfde6851687723d3db
Nick Krichevsky <nick@ollien.com>
2025-06-17 22:41:05 -0400

    Note where I got the gleam syntax workaround

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,9 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+
 	opts = function(opts)
 		-- stupid workaround for the fact that the gleam parser broke with the new nvim-treesitter
+		-- stolen from https://github.com/rawhat/config/blob/master/config/nvim/lua/plugins/treesitter.lua#L94
 		local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 		parser_config.gleam = {
 			install_info = {
