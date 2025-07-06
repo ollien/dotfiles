05d94d9d157ae75b1d142f305f63cfd2b455e45a
Nick Krichevsky <nick@ollien.com>
2025-07-06 09:30:23 -0400

    Add fugitive and change gitlinker fork

--- dot_config/nvim/lua/plugins/gitlinker.lua
+++ dot_config/nvim/lua/plugins/gitlinker.lua
@@ -1,8 +1,11 @@
 return {
-	"ruifm/gitlinker.nvim",
-	dependencies = {
-		{ "nvim-lua/plenary.nvim" },
+	{
+		"linrongbin16/gitlinker.nvim",
+		cmd = "GitLink",
+		opts = {},
+		keys = {
+			{ "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
+			{ "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+		},
 	},
-	opts = {},
-	keys = { "<leader>gy" },
 }
