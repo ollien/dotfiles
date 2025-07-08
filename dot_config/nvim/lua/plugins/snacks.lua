bfd488544683c481d413f135c682f255d11920e6
Nick Krichevsky <nick@ollien.com>
2025-07-08 15:04:23 -0400

    use vscode layout for ui select

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -7,6 +7,23 @@ return {
 			layout = {
 				preset = "vscode",
 			},
+			sources = {
+				select = {
+					config = function(opts)
+						vim.print(opts.layout.layout)
+						local max = vim.o.lines * 0.8 - 10
+						-- Hack: this is to work around these two issues
+						-- https://github.com/folke/snacks.nvim/issues/2035
+						-- https://github.com/folke/snacks.nvim/issues/902#issuecomment-2649284650
+						-- the vscode layout has an extra 0.5 of height, so we take the existing max
+						-- and use that
+						opts.layout.layout.height = math.min(max, opts.layout.layout.height + 0.5)
+					end,
+					layout = {
+						preset = "vscode",
+					},
+				},
+			},
 			matcher = {
 				frecency = true,
 				history_bonus = true,
