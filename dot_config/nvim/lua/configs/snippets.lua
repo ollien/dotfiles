26a9cc8cc1d4df013a4c7831a1c463ae93349ce2
Nick Krichevsky <nick@ollien.com>
2025-09-21 13:59:47 -0400

    Add do snippet back for elixir

--- dot_config/nvim/lua/configs/snippets.lua
+++ dot_config/nvim/lua/configs/snippets.lua
@@ -1,1 +1,22 @@
 vim.g.vscode_snippets_exclude = { "elixir" }
+
+local luasnip = require("luasnip")
+local snippet = luasnip.snippet
+local insert_node = luasnip.insert_node
+local format = require("luasnip.extras.fmt").fmt
+
+luasnip.add_snippets("elixir", {
+	snippet(
+		"do",
+		format(
+			[[
+        do
+          {}
+        end
+    ]],
+			{
+				insert_node(1),
+			}
+		)
+	),
+})
