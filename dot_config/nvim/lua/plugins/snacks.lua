6426fce4370757809203f243a9ed2380b353f850
Nick Krichevsky <nick@ollien.com>
2025-12-24 12:31:05 -0500

    Fix code actions in LSP

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -15,13 +15,16 @@ return {
 			sources = {
 				select = {
 					config = function(opts)
-						local max = math.floor(vim.o.lines * 0.8 - 10)
-						-- Hack: this is to work around these two issues
-						-- https://github.com/folke/snacks.nvim/issues/2035
-						-- https://github.com/folke/snacks.nvim/issues/902#issuecomment-2649284650
-						-- the vscode layout has an extra 0.5 of height, so we take the existing max
-						-- and use that
-						opts.layout.layout.height = math.min(max, opts.layout.layout.height + 0.5)
+						-- Not all actions have this property for some reason?
+						if opts.layout.layout then
+							local max = math.floor(vim.o.lines * 0.8 - 10)
+							-- Hack: this is to work around these two issues
+							-- https://github.com/folke/snacks.nvim/issues/2035
+							-- https://github.com/folke/snacks.nvim/issues/902#issuecomment-2649284650
+							-- the vscode layout has an extra 0.5 of height, so we take the existing max
+							-- and use that
+							opts.layout.layout.height = math.min(max, opts.layout.layout.height + 0.5)
+						end
 					end,
 					layout = {
 						preset = "vscode",
