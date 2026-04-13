fe95a53448585c225cbf31dc6b64798b8b63f654
Nick Krichevsky <nick@ollien.com>
2026-04-12 21:54:08 -0400

    Remove incorrect default for with_local

--- dot_config/nvim/lua/configutil/with_local.lua
+++ dot_config/nvim/lua/configutil/with_local.lua
@@ -3,8 +3,9 @@ local lua_dir = vim.fs.joinpath(vim.fn.stdpath("config"), "lua")
 local function load_config(relative_path)
 	local ok, result = pcall(dofile, vim.fs.joinpath(lua_dir, relative_path))
 	if not ok then
-		return { opts = {} }
+		return {}
 	end
+
 	if type(result) ~= "table" then
 		error("Config module '" .. relative_path .. "' must return a table")
 	end
@@ -13,7 +14,7 @@ end
 
 return function(path, opts)
 	opts = opts or {}
-	local base_config = load_config(path)
+	local base_config = opts.base_config or load_config(path)
 	local local_config = load_config(vim.fs.joinpath("_local", path))
 	if opts.list then
 		return vim.list_extend(vim.deepcopy(base_config), local_config)
