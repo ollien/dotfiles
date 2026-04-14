383c7a39259469fcde0b03855d3be42592c93e91
Nick Krichevsky <nick@ollien.com>
2026-04-13 20:54:55 -0400

    Add buffer completions to blink

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -37,6 +37,7 @@ vim.o.cursorline = true
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
+vim.g.sleuth_defaults = "sw=4"
 
 vim.o.spell = true
 vim.o.spelloptions = "camel"
--- dot_config/nvim/lua/plugins/blink.cmp.lua
+++ dot_config/nvim/lua/plugins/blink.cmp.lua
@@ -49,10 +49,14 @@ return {
 			documentation = { auto_show = true, auto_show_delay_ms = 500 },
 		},
 		sources = {
-			default = { "lsp", "path", "snippets" },
+			default = { "lsp", "buffer", "path", "snippets" },
 			providers = {
+				buffer = {
+					async = true,
+				},
 				lsp = {
 					async = true,
+					fallbacks = {},
 				},
 			},
 		},
