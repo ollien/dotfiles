5c0995cf63474563c5d85b56b70e83eff7eaeec9
Nick Krichevsky <nick@ollien.com>
2026-04-13 22:21:08 -0400

    Make fallback listchars more similar to dynamic ones

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -28,7 +28,7 @@ vim.o.splitright = true
 vim.o.splitbelow = true
 
 vim.o.list = true
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.listchars = { tab = "›  ", trail = "·", nbsp = "␣" }
 
 vim.o.inccommand = "nosplit"
 vim.o.scrolloff = 10
