f0ea4bea806e8bc5e81e1ab681d2e158c09627dd
Nick Krichevsky <nick@ollien.com>
2026-04-13 22:20:58 -0400

    Replace vim-sleuth with guess-indent.nvim

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -3,6 +3,7 @@ local plugin = require("configutil.plugin")
 
 local plugins = {
 	plugin("Bekaboo/dropbar.nvim"),
+	plugin("catppuccin/nvim"),
 	plugin("coffebar/neovim-project"),
 	plugin("FabijanZulj/blame.nvim"),
 	plugin("folke/snacks.nvim"),
@@ -16,15 +17,14 @@ local plugins = {
 	plugin("MagicDuck/grug-far.nvim"),
 	plugin("mason-org/mason.nvim"),
 	plugin("neovim/nvim-lspconfig"),
+	plugin("NMAC427/guess-indent.nvim"),
 	plugin("nvim-lualine/lualine.nvim"),
 	plugin("nvim-mini/mini.nvim"),
 	plugin("nvim-tree/nvim-tree.lua"),
 	plugin("nvim-tree/nvim-web-devicons"),
-	plugin("catppuccin/nvim"),
 	plugin("romus204/tree-sitter-manager.nvim"),
 	plugin("saghen/blink.cmp"),
 	plugin("stevearc/conform.nvim"),
-	plugin("tpope/vim-sleuth"),
 }
 
 return with_local("plugins.lua", { list = true, base_config = plugins })
