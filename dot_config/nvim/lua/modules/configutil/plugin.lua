9d62c15c23afdbdf02abd12dd8618ca65431a721
Nick Krichevsky <nick@ollien.com>
2026-04-14 20:53:02 -0400

    Move configutil into a module

---
+++ dot_config/nvim/lua/modules/configutil/plugin.lua
@@ -0,0 +1,7 @@
+local with_local = require("modules.configutil.with_local")
+
+return function(name)
+	local short_name = name:match(".+/(.+)") or error("Invalid plugin name: " .. name)
+	local config = with_local(vim.fs.joinpath("plugins", short_name .. ".lua"), { default = { opts = {} } })
+	return vim.tbl_extend("force", { name }, config)
+end
---
+++ dot_config/nvim/lua/modules/configutil/with_local.lua
@@ -0,0 +1,26 @@
+local lua_dir = vim.fs.joinpath(vim.fn.stdpath("config"), "lua")
+
+local function load_config(relative_path, default)
+	local ok, result = pcall(dofile, vim.fs.joinpath(lua_dir, relative_path))
+	if not ok then
+		return default
+	end
+
+	if type(result) ~= "table" then
+		error("Config module '" .. relative_path .. "' must return a table")
+	end
+	return result
+end
+
+return function(path, opts)
+	opts = opts or {}
+
+	local default = opts.default or {}
+	local base_config = opts.base_config or load_config(path, default)
+	local local_config = load_config(vim.fs.joinpath("_local", path), default)
+	if opts.list then
+		return vim.list_extend(vim.deepcopy(base_config), local_config)
+	else
+		return vim.tbl_deep_extend("force", base_config, local_config)
+	end
+end
