d231e5740455ad1e74af21f8b72550837c59bfd1
Nick Krichevsky <nick@ollien.com>
2026-04-14 20:21:55 -0400

    make nordic brighter

--- dot_config/nvim/lua/plugins/nordic.nvim.lua
+++ dot_config/nvim/lua/plugins/nordic.nvim.lua
@@ -1,13 +1,20 @@
 return {
 	lazy = false,
 	priority = 1000,
-	config = function()
+	---@module 'nordic'
+	---@type NordicOptions
+	opts = {
+		on_palette = function(_) end,
+		after_palette = function(_) end,
+		on_highlight = function(highlights, palette)
+			highlights.Delimiter = { fg = palette.blue1 }
+		end,
+
+		visual = { theme = "light" },
+	},
+	config = function(_, opts)
+		require("nordic").setup(opts)
+
 		vim.cmd.colorscheme("nordic")
-
-		local colors = require("nordic.colors")
-
-		vim.api.nvim_set_hl(0, "Search", { bg = colors.gray5, fg = colors.fg_bright, bold = true })
-		vim.api.nvim_set_hl(0, "IncSearch", { bg = colors.yellow.base, fg = colors.black0, bold = true })
-		vim.api.nvim_set_hl(0, "CurSearch", { link = "IncSearch" })
 	end,
 }
