b996825c7bdcd0fc2a8f1e06d143d92198e00eb9
Nick Krichevsky <nick@ollien.com>
2026-04-18 16:09:10 -0400

    Move lsp mappings into mappings.lua

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -44,3 +44,18 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 	end,
 })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+	callback = function(event)
+		local map = function(keys, func, desc, mode)
+			mode = mode or "n"
+			vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+		end
+		map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
+		map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
+		map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+		map("gd", vim.lsp.buf.definition, "[G]oto [d]eclaration")
+		map("gD", vim.lsp.buf.declaration, "[G]oto [d]eclaration")
+	end,
+})
--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -16,19 +16,6 @@ return {
 		"j-hui/fidget.nvim",
 	},
 	config = function()
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
-			callback = function(event)
-				local map = function(keys, func, desc, mode)
-					mode = mode or "n"
-					vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-				end
-				map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
-				map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
-				map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-			end,
-		})
-
 		---@type table<string, vim.lsp.Config>
 		local servers = with_local("config/lsp.lua")
 		servers = filter_servers(servers)
