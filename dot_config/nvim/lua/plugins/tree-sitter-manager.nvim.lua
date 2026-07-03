a5065f7227759819aede8641267aa9e978ee606b
Nick Krichevsky <nick@ollien.com>
2026-07-03 12:41:35 -0400

    Fix tree-sitter manager config

--- dot_config/nvim/lua/plugins/tree-sitter-manager.nvim.lua
+++ dot_config/nvim/lua/plugins/tree-sitter-manager.nvim.lua
@@ -1,13 +1,17 @@
 local with_local = require("modules.configutil.with_local")
-
 return {
 	event = { "BufReadPre", "VeryLazy" },
 	config = function()
 		local languages = with_local("config/treesitter.lua")
-
+		local sourced_languages = {}
+		for lang, config in pairs(languages) do
+			if not vim.tbl_isempty(config) then
+				sourced_languages[lang] = config
+			end
+		end
 		require("tree-sitter-manager").setup({
 			ensure_installed = vim.tbl_keys(languages),
-			languages = languages,
+			languages = sourced_languages,
 			auto_install = true,
 		})
 	end,
